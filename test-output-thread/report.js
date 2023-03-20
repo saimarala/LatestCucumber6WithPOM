643dcc1624c1fd457b9d389fd2e26e0292d82fd1
saimarala <saiautomation20@gmail.com>
2023-03-20 15:46:08 +0530

    FM

---
+++ test-output-thread/report.js
@@ -0,0 +1,153 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "77291836-0415-463a-b44a-57a5fb55ac0b",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1678017185243,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1678017185259,
+    "className": "undefined"
+  },
+  {
+    "id": "56222a2b-8560-407e-a86a-c89bc5730aa6",
+    "feature": "Contact Us Feature",
+    "scenario": "Contact Us scenario with different set of data",
+    "start": 1678017184931,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1678017184962,
+    "className": "undefined"
+  },
+  {
+    "id": "0856a151-d32b-44f6-9263-ba1f1a8f3437",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts section count",
+    "start": 1678017184898,
+    "group": 1,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1678017184914,
+    "className": "undefined"
+  },
+  {
+    "id": "5cfe3b9e-32c8-4c04-bc90-b46a716bf50c",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password link",
+    "start": 1678017185205,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1678017185210,
+    "className": "undefined"
+  },
+  {
+    "id": "c9992762-b93b-4f49-a8c4-28e7fdff5a2a",
+    "feature": "Contact Us Feature",
+    "scenario": "Contact Us scenario with different set of data",
+    "start": 1678017185160,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1678017185171,
+    "className": "undefined"
+  },
+  {
+    "id": "ba02387d-a5f0-4cb0-8b12-821d1cd95293",
+    "feature": "Contact Us Feature",
+    "scenario": "Contact Us scenario with different set of data",
+    "start": 1678017184990,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1678017185014,
+    "className": "undefined"
+  },
+  {
+    "id": "c6ad4d5b-cd5c-48e8-9cc7-e07b1db1e972",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1678017185180,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1678017185198,
+    "className": "undefined"
+  },
+  {
+    "id": "08def2b0-e049-4ed6-a1b4-2fd189745e4d",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts page title",
+    "start": 1678017185268,
+    "group": 1,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1678017185283,
+    "className": "undefined"
+  },
+  {
+    "id": "6563867e-87cb-4f01-9411-eaa272fbaf21",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts section count",
+    "start": 1678017185297,
+    "group": 1,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1678017185364,
+    "className": "undefined"
+  },
+  {
+    "id": "609604e0-9693-4e76-8669-84b3222db943",
+    "feature": "Contact Us Feature",
+    "scenario": "Contact Us scenario with different set of data",
+    "start": 1678017185064,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1678017185092,
+    "className": "undefined"
+  },
+  {
+    "id": "355f6862-5890-499c-8355-47cab7c4d2c8",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts page title",
+    "start": 1678017184644,
+    "group": 1,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1678017184844,
+    "className": "undefined"
+  },
+  {
+    "id": "54c92229-6fa7-44c9-bf60-87995b4b7085",
+    "feature": "Contact Us Feature",
+    "scenario": "Contact Us scenario with different set of data",
+    "start": 1678017185027,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1678017185046,
+    "className": "undefined"
+  },
+  {
+    "id": "0c9f6446-2407-4045-a2a7-820f10eb9964",
+    "feature": "Contact Us Feature",
+    "scenario": "Contact Us scenario with different set of data",
+    "start": 1678017185111,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1678017185129,
+    "className": "undefined"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
